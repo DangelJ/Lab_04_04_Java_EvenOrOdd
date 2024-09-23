58836b5887566b0a11cce1da724be3c7df38992a
Jonah <dangelja@mail.uc.edu>
2024-09-23 19:33:32 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int value = 5;
+
+        if(value % 2 == 0){
+            System.out.println("Your number is even!");
+        } else {
+            System.out.println("Your number is odd");
+        }
+
+
+    }
+}
